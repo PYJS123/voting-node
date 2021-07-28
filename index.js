673cd869e7a7a2a49e26987d4d0b935a120a7452
PYJS123 <bibi.abrah@gmail.com>
2021-07-28 16:13:58 +0100

    Gotten buttons ready, pages ready, moving onto charts and database details

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Datastore = require('nedb');
+const colours = [["black", true], ["white", false], ["red", true], ["green", true], ["yellow", false], ["blue", true], ["pink", false], ["gray", false], ["brown", true], ["orange", false], ["purple", true]];
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
@@ -8,3 +9,15 @@ app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
+
+app.get('/cols', (req, res) => {
+  res.json({
+    cols: colours
+  });
+});
+
+app.post('/postdata', (req, res) => {
+  const data = request.body;  // Includes data.upticks and data.col
+  db.update({ elem: data.col }, { $inc: { votes: data.upticks } }, { multi: true }, function (err, numReplaced) {});
+  res.end();
+});
