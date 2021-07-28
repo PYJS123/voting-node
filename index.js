45a33a36ae6e04a0a5334bdedbb6ef6fc54a62d7
PYJS123 <bibi.abrah@gmail.com>
2021-07-28 17:58:11 +0100

    Debugged Glitch error with dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const Datastore = require('nedb');
 const colours = [["black", true], ["white", false], ["red", true], ["green", true], ["yellow", false], ["blue", true], ["pink", false], ["gray", false], ["brown", true], ["orange", false], ["purple", true]];
+require('dotenv').config();
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
